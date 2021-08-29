3c1b591109430a210c5228dc3c5400da86acc44e
KOTLnt <hoangvietanh60@gmail.com>
2021-08-29 06:42:53 -0500

    aaaa
    aaa

---
+++ main.js
@@ -0,0 +1,20 @@
+// var arr = [16,32,-64,1];
+// var newArr = [];
+
+// for(let index = 0; index < arr.length; index++) {
+//   var num;
+//   for(var i = 2; i<=arr[index]; i++){
+//     if(Number.isInteger(Math.pow(arr[index], 1/i))){
+//       num = Math.pow(arr[index], 1/i);
+//       newArr.push(arr[index]);
+//       break;
+//     }
+//   }
+  
+// }
+// console.log(newArr);
+
+
+
+
+
